4fc7db1df7bee45340cb1ba02f62ab5482730508
KNH <reiraps@naver.com>
2023-12-18 19:52:52 +0900

    [추가] save & load system
    기존 VictoryDefeatCondition cs에 새롭게 json을 이용한 세이브 로드 기능을 추가

--- Assets/Scripts/3. NamHyeon/VictoryDefeatCondition.cs
+++ Assets/Scripts/3. NamHyeon/VictoryDefeatCondition.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
 using UnityEngine;
 
 public class VictoryDefeatCondition : MonoBehaviour
@@ -11,6 +11,16 @@ public class VictoryDefeatCondition : MonoBehaviour
     private int CurrentEnemyCount = 0;
 
     private bool GameEnded = false;
+
+    private string SaveFile;
+
+
+    private void Start()
+    {
+        SaveFile = Path.Combine(Application.persistentDataPath, "GameSave.json");
+
+        LoadGame();
+    }
 
 
     public void EnemyKilled()
@@ -39,9 +49,39 @@ public class VictoryDefeatCondition : MonoBehaviour
         }
     }
 
+    
+    private void SaveGame()
+    {
+        GameData gameData = new GameData(/* 저장 데이터 */);
+        string jsonString = JsonUtility.ToJson(gameData);
+
+        File.WriteAllText(SaveFile, jsonString);
+    }
+
+    private void LoadGame()
+    {
+        if (File.Exists(SaveFile))
+        {
+            string jsonString = File.ReadAllText(SaveFile);
+            GameData gameData = JsonUtility.FromJson<GameData>(jsonString);
+
+            //게임 데이터 적용
+        }
+    }
+
+    
+
+
     private void GameEnd()
     {
         GameEnded = true;
+    }
+
+
+    [System.Serializable]
+    private class GameData
+    {
+        //저장할 데이터 추가(플레이어체력,위치,점수 등)
     }
 
 }
