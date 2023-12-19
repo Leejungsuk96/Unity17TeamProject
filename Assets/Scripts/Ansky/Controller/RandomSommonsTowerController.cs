fc5f8ae1be69f29a1b586d9f07a1ca20fd00b62c
angskyee <149351121+angskyee@users.noreply.github.com>
2023-12-19 16:22:12 +0900

    update
    램던 생성 다시

--- Assets/Scripts/Ansky/Controller/RandomSommonsTowerController.cs
+++ Assets/Scripts/Ansky/Controller/RandomSommonsTowerController.cs
@@ -31,7 +31,7 @@ public class RandomSommonsTowerController : MonoBehaviour
     {
         Debug.Log("aaa");
         int randomTowerIndex = Random.Range(0, summonsTowersPrefabs.Count);
-        GameObject summonsTower = Instantiate(summonsTowersPrefabs[0], summonsTowersPositions[0].position, Quaternion.identity);
+        GameObject summonsTower = Instantiate(summonsTowersPrefabs[randomTowerIndex], summonsTowersPositions[0].position, Quaternion.identity);
         summonsTower.transform.parent = transform;
         currentSommonsCount++;
     }
