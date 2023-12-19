f381031c5cbc761becdda0f3837b43cf4a39ee0a
Leejungsuk96 <svwsv3462@gmail.com>
2023-12-19 10:37:56 +0900

    [추가]적 움직임

--- Assets/Scripts/MainSceneScripts/Spawner.cs
+++ Assets/Scripts/MainSceneScripts/Spawner.cs
@@ -7,20 +7,23 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
-
-    float timer;
+    float timer;        
 
     private void Awake()
     {
         spawnPoint = GetComponentsInChildren<Transform>(); // 스폰 포인트에서 몬스터 생성
+    }
+    private void Start()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-
-        if(timer > 1f) // 몬스터 스폰 시간
+        
+        if (timer > 1f) // 몬스터 스폰 시간
         {
             timer = 0;
             Spwan();
@@ -32,4 +35,5 @@ public class Spawner : MonoBehaviour
         GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,3));
         enemy.transform.position = spawnPoint[1].position;
     }
+        
 }
