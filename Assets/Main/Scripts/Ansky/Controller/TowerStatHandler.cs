9646afb380201a1b72176e9d55914fcd94123630
angskyee <149351121+angskyee@users.noreply.github.com>
2023-12-20 17:05:17 +0900

    Upgrade
    타워 업그레이드 준비

--- Assets/Main/Scripts/Ansky/Controller/TowerStatHandler.cs
+++ Assets/Main/Scripts/Ansky/Controller/TowerStatHandler.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 
 public class TowerStatHandler : MonoBehaviour
 {
+    private const float MinAttackDelay = 0.03f;
+    private const float MinAttackPower = 0.5f;
+    private const float MinAttackSize = 0.4f;
+    private const float MinAttackSpeed = .1f;
+
+    private const float MinSpeed = 0.8f;
+
+    private const int MinMaxHealth = 5;
+
 
     [SerializeField] private TowerStats baseStats;
+    public TowerStats CurrentStates { get; private set; }
+    public List<TowerStats> statsModifiers  = new List<TowerStats>();
+
     public TowerStats CurrentStats { get; private set; }
 
     private void Awake()
@@ -24,11 +36,126 @@ public class TowerStatHandler : MonoBehaviour
         }
 
         CurrentStats = new TowerStats { attackSO = attackSO };
-        CurrentStats.statsChangeType = baseStats.statsChangeType;
-        CurrentStats.maxHealth = baseStats.maxHealth;
-        CurrentStats.speed = baseStats.speed;
-        CurrentStats.spawnSpeed = baseStats.spawnSpeed;
+        UpdateStats((a, b) => b, baseStats);
+        if (CurrentStats.attackSO != null)
+        {
+            CurrentStats.attackSO.target = baseStats.attackSO.target;
+        }
 
+        foreach (TowerStats modifier in statsModifiers.OrderBy(o => o.statsChangeType))
+        {
+            if (modifier.statsChangeType == StatsChangeType.Override)
+            {
+                UpdateStats((o, o1) => o1, modifier);
+            }
+            else if (modifier.statsChangeType == StatsChangeType.Add)
+            {
+                UpdateStats((o, o1) => o + o1, modifier);
+            }
+            else if (modifier.statsChangeType == StatsChangeType.Multiple)
+            {
+                UpdateStats((o, o1) => o * o1, modifier);
+            }
+        }
 
+        LimitAllStats();
+
+    }
+
+    private void UpdateStats(Func<float, float, float> operation, TowerStats newModifier)
+    {
+        CurrentStats.maxHealth = (int)operation(CurrentStats.maxHealth, newModifier.maxHealth);
+        CurrentStats.speed = operation(CurrentStats.speed, newModifier.speed);
+        CurrentStats.spawnSpeed = operation(CurrentStats.spawnSpeed, newModifier.spawnSpeed);
+
+        if (CurrentStats.attackSO == null || newModifier.attackSO == null)
+            return;
+
+        UpdateAttackStats(operation, CurrentStats.attackSO, newModifier.attackSO);
+
+        if (CurrentStats.attackSO.GetType() != newModifier.attackSO.GetType())
+        {
+            return;
+        }
+
+        switch (CurrentStats.attackSO)
+        {
+            case RangedAttackData _:
+                ApplyRangedStats(operation, newModifier);
+                break;
+        }
+    }
+
+    public void AddStatModifier(TowerStats statModifier)
+    {
+        statsModifiers.Add(statModifier);
+        UpdateTowerStats();
+    }
+
+    public void RemoveStatModifier(TowerStats statModifier)
+    {
+        statsModifiers.Remove(statModifier);
+        UpdateTowerStats();
+    }
+
+    private void UpdateAttackStats(Func<float, float, float> operation, AttackSO currentAttack, AttackSO newAttack)
+    {
+        if (currentAttack == null || newAttack == null)
+        {
+            return;
+        }
+
+        currentAttack.delay = operation(currentAttack.delay, newAttack.delay);
+        currentAttack.power = operation(currentAttack.power, newAttack.power);
+        currentAttack.size = operation(currentAttack.size, newAttack.size);
+        currentAttack.speed = operation(currentAttack.speed, newAttack.speed);
+    }
+
+    private void ApplyRangedStats(Func<float, float, float> operation, TowerStats newModifier)
+    {
+        RangedAttackData currentRangedAttacks = (RangedAttackData)CurrentStats.attackSO;
+
+        if (!(newModifier.attackSO is RangedAttackData))
+        {
+            return;
+        }
+
+        RangedAttackData rangedAttacksModifier = (RangedAttackData)newModifier.attackSO;
+        currentRangedAttacks.multipleProjectilesAngel =
+            operation(currentRangedAttacks.multipleProjectilesAngel, rangedAttacksModifier.multipleProjectilesAngel);
+        currentRangedAttacks.spread = operation(currentRangedAttacks.spread, rangedAttacksModifier.spread);
+        currentRangedAttacks.duration = operation(currentRangedAttacks.duration, rangedAttacksModifier.duration);
+        currentRangedAttacks.numberofProjectilesPerShot = Mathf.CeilToInt(operation(currentRangedAttacks.numberofProjectilesPerShot,
+            rangedAttacksModifier.numberofProjectilesPerShot));
+        currentRangedAttacks.projectileColor = UpdateColor(operation, currentRangedAttacks.projectileColor, rangedAttacksModifier.projectileColor);
+    }
+
+    private Color UpdateColor(Func<float, float, float> operation, Color currentColor, Color newColor)
+    {
+        return new Color(
+            operation(currentColor.r, newColor.r),
+            operation(currentColor.g, newColor.g),
+            operation(currentColor.b, newColor.b),
+            operation(currentColor.a, newColor.a));
+    }
+
+    private void LimitStats(ref float stat, float minVal)
+    {
+        stat = Mathf.Max(stat, minVal);
+    }
+
+    private void LimitAllStats()
+    {
+        if (CurrentStats == null || CurrentStats.attackSO == null)
+        {
+            return;
+        }
+
+        LimitStats(ref CurrentStats.attackSO.delay, MinAttackDelay);
+        LimitStats(ref CurrentStats.attackSO.power, MinAttackPower);
+        LimitStats(ref CurrentStats.attackSO.size, MinAttackSize);
+        LimitStats(ref CurrentStats.attackSO.speed, MinAttackSpeed);
+        LimitStats(ref CurrentStats.speed, MinSpeed);
+        CurrentStats.maxHealth = Mathf.Max(CurrentStats.maxHealth, MinMaxHealth);
     }
 }
